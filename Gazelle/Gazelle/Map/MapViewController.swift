38f3ee9c643a8fb2a50bbd5ca3bba9af076419b9
Angela Montez <angelali.montez@gmail.com>
2023-04-17 19:03:10 -0500

    Zoom into user location

--- Gazelle/Gazelle/Map/MapViewController.swift
+++ Gazelle/Gazelle/Map/MapViewController.swift
@@ -9,6 +9,7 @@
 //  https://medium.com/swlh/how-to-work-with-mapkit-5c96c45f32f0
 //  https://medium.com/@kiransjadhav111/corelocation-map-kit-get-the-users-current-location-set-a-pin-in-swift-edb12f9166b2
 //  https://www.johncodeos.com/how-to-display-location-and-routes-with-corelocation-mapkit-using-swift/
+//  https://medium.com/@pravinbendre772/search-for-places-and-display-results-using-mapkit-a987bd6504df
 
 import UIKit
 import MapKit
@@ -20,7 +21,38 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
     }
 
+}
+
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            print("Not Determined")
+        case .restricted, .denied:
+            print("Restricted or Denied")
+        case .authorizedAlways, .authorizedWhenInUse:
+            print("Authorized")
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestLocation()
+        default:
+            print("Status Unknown")
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            print("Location:: \(location)")
+            let zoom = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let region = MKCoordinateRegion(center: location.coordinate, span: zoom)
+            mapView.setRegion(region, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error:: \(error)")
+    }
 }
