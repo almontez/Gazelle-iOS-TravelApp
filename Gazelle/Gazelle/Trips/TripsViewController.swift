46ea41854d1129f747dc11923e8f5e29365f5209
Angela Montez <angelali.montez@gmail.com>
2023-04-25 16:11:17 -0700

    Create Trips

---
+++ Gazelle/Gazelle/Trips/TripsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TripsViewController.swift
+//  Gazelle
+//
+//  Created by Angela Li Montez on 4/25/23.
+//
+
+import UIKit
+
+class TripsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
