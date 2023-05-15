3e45800e088067a35050194ccef80521ca57fad6
Angela Montez <angelali.montez@gmail.com>
2023-05-15 12:55:02 -0700

    Complete Location Details UI

--- Gazelle/Gazelle/Map/LocationDetailsController.swift
+++ Gazelle/Gazelle/Map/LocationDetailsController.swift
@@ -7,17 +7,52 @@
 
 import UIKit
 import MapKit
+import Contacts
 
 class LocationDetailsController: UITableViewController {
-
-    @IBOutlet weak var detailsTable: UITableView!
+    
+    @IBOutlet weak var placePhoneLabel: UILabel!
+    @IBOutlet weak var placeWebsiteLabel: UILabel!
+    @IBOutlet weak var placeAddressLabel: UILabel!
+    @IBOutlet weak var mapView: MKMapView!
+    
+    private var mapItem: MKMapItem?
+    private var boundingRegion: MKCoordinateRegion?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateDisplayedData()
     }
     
-    func display(_ mapItem: MKMapItem!, in region: MKCoordinateRegion) {
+    private func updateDisplayedData() {
+        guard isViewLoaded, let mapItem = self.mapItem, let region = boundingRegion
+            else { return }
+        
         navigationItem.title = mapItem.name
+        
+        placeAddressLabel.text = mapItem.placemark.formattedAddress
+        placePhoneLabel.text = mapItem.phoneNumber
+        placeWebsiteLabel.text = mapItem.url?.absoluteString
+        
+        mapView.showAnnotations([mapItem.placemark], animated: false)
+        mapView.region = region
+    }
+    
+    @IBAction func openItemInMaps(_ sender: UIButton) {
+        mapItem?.openInMaps(launchOptions: nil)
     }
 
+    func display(_ mapItem: MKMapItem!, in region: MKCoordinateRegion) {
+        self.mapItem = mapItem
+        self.boundingRegion = region
+        updateDisplayedData()
+    }
+
+}
+
+extension MKPlacemark {
+    var formattedAddress: String? {
+        guard let postalAddress = postalAddress else { return nil }
+        return CNPostalAddressFormatter.string(from: postalAddress, style: .mailingAddress).replacingOccurrences(of: "\n", with: " ")
+    }
 }
